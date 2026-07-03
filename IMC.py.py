1715a158d5f66da7c445a09be260cf30fbf82e3c
Mr.K <keoulendembele@gmail.com>
2026-07-03 17:03:54 +0200

    Enhance IMC calculation with rounding
    Added rounding to the IMC calculation for better precision.

--- IMC.py.py
+++ IMC.py.py
@@ -3,5 +3,6 @@ def IMC():
     weight = float(input("Enter your weight in kilograms: "))
     height = float(input("Enter your height in cm (ex:179) : "))
     IMC = weight / (height / 100) ** 2
+    IMC = round(IMC,2)
     print ("hello that your IMC:", IMC)
 IMC()
